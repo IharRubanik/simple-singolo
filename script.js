51849b0186805e401dfaa0b0b530c9a9ed6898c3
IharRubanik <74908305+IharRubanik@users.noreply.github.com>
2021-03-26 01:34:40 +0300

    fix: mobile slider

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ let btnLeft = document.getElementById('btn_left');
 let btnRight = document.getElementById('btn_right');
 let slide1 = document.getElementById('slide_red');
 let slide2 = document.getElementById('slide_blue');
+let menuMobile = document.getElementById('menu_mobile');
+let threeStripes = document.getElementById('three_stripes');
+let nameStar = document.getElementById('headr__name_star');
+let nameMobile = document.getElementById('name_mobile');
 
 
 btnRight.addEventListener('click', nextSlide);
 btnLeft.addEventListener('click', nextSlide);
+menuMobile.addEventListener('click', openMenu);
 
 
 function nextSlide() {
@@ -17,3 +22,8 @@ function nextSlide() {
         slide2.style.opacity = '0';
     }
 }
+function openMenu() {
+    threeStripes.className = ('main_menu_active')
+    nameStar.style.display = "none"
+    nameMobile.style.display = "none"
+}   
